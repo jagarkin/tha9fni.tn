b0dfbcc7db70779b91c6504168fee879f3c34197
Walee Sebiiy <44040669+jagarkin@users.noreply.github.com>
2021-04-11 12:47:34 +0100

    structure de projet

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
-import React, { useState } from 'react';
-// there's a problem withitn the react-router-dom 
-import { Navbar } from './components/navBar/navbar'
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router } from 'react-router-dom'
-import { motion } from 'framer-motion'
-import { Link } from 'react-scroll'
-
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
-  const [withMenu, setWithMenu] = useState(false);
   return (
- <Router>
-        <div className='body-container' >
-            <Navbar 
-                withMenu={withMenu}
-                setWithMenu={setWithMenu}
-            />
-            <div className='container-all-pages' onClick={() => withMenu === true ? setWithMenu(false) : null} >
-                <div className='wrapper-menu-mobile' onClick={() => setWithMenu(true)}>
-                    <img alt='menu'  src={MenuLogo}/>
-                </div>
-                
-                <div className='title-page' >
-                    <motion.h1 initial={true} animate={{ x: 300}} className='name'>
-                         THIS IS THA9FNI .
-                    </motion.h1>
-                    
-         </div></div></div></Router>
-  );
+    <>
+      <Router>
+        {/* switch between pages */}
+        <Switch></Switch>
+        {/* */}
+      </Router>
+    </>
+  )
 }
-export default App;
+export default App
